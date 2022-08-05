3ff9ba19a0a536299e407e4f2a6a4aeecfd40f4b
vamsi-stanford <vamsi99@stanford.edu>
2022-08-05 09:29:22 +0530

    finalized cleanup function for stats, and ran the script to generate clean data files

--- cleanup.py
+++ cleanup.py
@@ -3,8 +3,20 @@ import numpy as np
 import sys
 from cleanupUtils import *
 
-######### CLEANUP TESTING #########
+folders = ['season_stats_data/', 'advanced_stats_data/']
 
-fileName = 'season_stats_data/12_13_data.csv'
-rows = collapseData(fileName)
-print(rows[3:6])
+####### REMOVE OLD CLEAN FILES ######
+for folder in folders:
+    stats_files = os.listdir(folder)
+    for file in stats_files:
+        if 'clean' in file:
+            os.remove(folder + file)
+
+####### STATS CLEANUP #########
+
+for folder in folders:
+    stats_files = os.listdir(folder)
+    for file in stats_files:
+        newRows = collapseData(folder + file)
+        if newRows == 0:
+            print(f'Error cleaning up {folder + file}')
